21ed85327b6f58c5bb51c9284ef3aae4dba1b628
ODA <twbtarai.blackpepper@gmail.com>
2018-08-06 02:38:42 +0900

    add various things and update simulator

---
+++ logAnalyzer/logparser.cpp
@@ -0,0 +1,253 @@
+#include <sys/stat.h>
+#include <dirent.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+
+std::vector<std::string> split(std::string str, char del) {
+    int first = 0;
+    int last = str.find_first_of(del);
+ 
+    std::vector<std::string> result;
+ 
+    while (first < str.size()) {
+        std::string subStr(str, first, last - first);
+ 
+        result.push_back(subStr);
+ 
+        first = last + 1;
+        last = str.find_first_of(del, first);
+ 
+        if (last == std::string::npos) {
+            last = str.size();
+        }
+    }
+ 
+    return result;
+}
+
+int main(int argc,char *argv[]){
+    
+    vector<string> fileNames;
+    int i, dirElements;
+    string search_path;
+
+    struct stat stat_buf;
+    struct dirent **namelist=NULL;
+
+    long** cardWin = new long*[2];
+    cardWin[0] = new long[160];
+    cardWin[1] = new long[160];
+    long** cardLose = new long*[2];
+    cardLose[0] = new long[160];
+    cardLose[1] = new long[160];
+    int* cardCosts = new int[160];
+    for(int i = 0; i < 160; i ++){
+        cardWin[0][i] = 0;
+        cardLose[0][i] = 0;
+        cardWin[1][i] = 0;
+        cardLose[1][i] = 0;
+    }
+    long** manaCurveWin = new long* [13];
+    long** manaCurveLose = new long* [13];
+    for(int i = 0; i < 13; i ++){
+        manaCurveWin[i] = new long[31];
+        manaCurveLose[i] = new long[31];
+    }
+
+    int* playerWin = new int[2];
+    playerWin[0] = 0;
+    playerWin[1] = 0;
+
+    // parse cardlist
+    std::ifstream ifs("cardlist.txt");
+    std::string str;
+    if (ifs.fail())
+    {
+        std::cerr << "失敗" << std::endl;
+        return -1;
+    }
+    int id = 0;
+    while (getline(ifs, str))
+    {
+        vector<string> result = split(str, ';');
+        cardCosts[id] = std::atoi(result[3].c_str());
+        id ++;
+    }
+
+    string searchDir = argv[1];
+    
+
+    // dirElements にはディレクトリ内の要素数が入る
+    dirElements = scandir(searchDir.c_str(), &namelist, NULL, NULL);
+
+    if(dirElements == -1) {
+        cout << "ERROR" <<  endl;
+    }
+
+    else{
+
+        //ディレクトリかファイルかを順番に識別
+        for (i=0; i<dirElements; i+=1) {
+            int index = string(namelist[i]->d_name).find("result-");
+            if(index != std::string::npos){
+                string matchID = string(namelist[i]->d_name).substr(index + 7, 12);
+                int winPlayer;
+
+                // load result
+                std::ifstream ifs(searchDir + "/result-" + matchID + ".txt");
+                if (ifs.fail())
+                {
+                    std::cerr << "失敗" << std::endl;
+                    return -1;
+                }
+                while (getline(ifs, str))
+                {
+                    vector<string> result = split(str, ' ');
+                    if(result[0] == "1"){
+                        winPlayer = 1;
+                        playerWin[0] ++;
+                    }
+                    else{
+                        winPlayer = 2;
+                        playerWin[1] ++;
+                    }
+                }
+                // load final deck player 1
+                std::ifstream ifs1(searchDir + "/player1-" + matchID + ".txt");
+                if (ifs1.fail())
+                {
+                    std::cerr << "失敗" << std::endl;
+                    return -1;
+                }
+                int lineNo = 0;
+                
+                vector<string> deckString;
+                while (getline(ifs1, str))
+                {
+                    lineNo ++;
+                    if(lineNo == 88){
+                        deckString = split(str, ' ');
+                    }
+                    if(lineNo == 90){
+                        int* manaCurve = new int[13];
+                        for(int i = 0; i < 13; i ++){
+                            manaCurve[i] = 0;
+                        }
+                        for(int i = 0; i < 160; i ++){
+                            int cnt = std::atoi(deckString[i].c_str());
+                            if(i == std::atoi(str.c_str()) - 1){
+                                cnt ++;
+                            }
+                            manaCurve[cardCosts[i]] += cnt;
+                            if(winPlayer == 1){
+                                cardWin[0][i] += cnt;
+                            }
+                            else{
+                                cardLose[0][i] += cnt;
+                            }
+                        }
+                        for(int i = 0; i < 13; i ++){
+                            if(winPlayer == 1){
+                                manaCurveWin[i][manaCurve[i]] += 1;
+                            }
+                            else{
+                                manaCurveLose[i][manaCurve[i]] += 1;
+                            }
+                        }
+                        break;
+                    }
+                }
+
+                // load final deck player 1
+                std::ifstream ifs2(searchDir + "/player2-" + matchID + ".txt");
+                if (ifs2.fail())
+                {
+                    std::cerr << "失敗" << std::endl;
+                    return -1;
+                }
+                lineNo = 0;
+                while (getline(ifs2, str))
+                {
+                    lineNo ++;
+                    if(lineNo == 88){
+                        deckString = split(str, ' ');
+                    }
+                    if(lineNo == 90){
+                        int* manaCurve = new int[13];
+                        for(int i = 0; i < 13; i ++){
+                            manaCurve[i] = 0;
+                        }
+                        for(int i = 0; i < 160; i ++){
+                            int cnt = std::atoi(deckString[i].c_str());
+                            if(i == std::atoi(str.c_str()) - 1){
+                                cnt ++;
+                            }
+                            manaCurve[cardCosts[i]] += cnt;
+                            if(winPlayer == 2){
+                                cardWin[1][i] += cnt;
+                            }
+                            else{
+                                cardLose[1][i] += cnt;
+                            }
+                        }
+                        for(int i = 0; i < 13; i ++){
+                            if(winPlayer == 2){
+                                manaCurveWin[i][manaCurve[i]] += 1;
+                            }
+                            else{
+                                manaCurveLose[i][manaCurve[i]] += 1;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    double avarageWinRate[2];
+    cout << "totalScan:" << (playerWin[0] + playerWin[1]) / 2 << endl;
+    for(int i = 0; i < 2; i ++){
+        avarageWinRate[i] = (double)playerWin[i] / (double)(playerWin[0] + playerWin[1]);
+    }
+    cout << avarageWinRate[0] << "/" << avarageWinRate[1] << endl;
+    for(int i = 0; i < 160; i ++){
+        /*double winRate1 = (double)cardWin[0][i] / (double)(cardWin[0][i] + cardLose[0][i]);
+        double winRate2 = (double)cardWin[1][i] / (double)(cardWin[1][i] + cardLose[1][i]);
+        cout << i + 1 << ":" << winRate1 - avarageWinRate[0] << "/" << winRate2 - avarageWinRate[1] << endl;*/
+        double winRate1 = (double)(cardWin[0][i] + cardWin[1][i]) / (double)(cardWin[0][i] + cardLose[0][i] + cardWin[1][i] + cardLose[1][i]);
+        cout << i + 1 << ":" << winRate1 - 0.5 << endl;
+    }
+
+    for(int i = 0; i < 160; i ++){
+        double winRate1 = (double)(cardWin[0][i] + cardWin[1][i]) / (double)(cardWin[0][i] + cardLose[0][i] + cardWin[1][i] + cardLose[1][i]);
+        cout << winRate1 - 0.5 << ",";
+    }
+
+    for(int i = 0; i < 13; i ++){
+        cout << "Mana:" << i  << endl;
+        for(int n = 0; n < 30; n ++){
+            double winRate = (double)(manaCurveWin[i][n]) / (double)(manaCurveWin[i][n] + manaCurveLose[i][n]);
+            cout << n << ":" << winRate << ",";
+        }
+        cout << endl;
+    }
+
+    int* totalManaCurve = new int[13];
+    for(int i = 0; i < 13; i ++){
+        totalManaCurve[i] = 0;
+    }
+    for(int i = 0; i < 160; i ++){
+        totalManaCurve[cardCosts[i]] ++;
+    }
+    for(int i = 0; i < 13; i ++){
+        cout << totalManaCurve[i] << ",";
+    }
+
+
+    free(namelist);
+}
