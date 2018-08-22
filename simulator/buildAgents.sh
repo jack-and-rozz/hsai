f90a566df7fca8f5e64c4f34b505e8ebe2f85662
Shoetsu Sato <shoetsu@tkl.iis.u-tokyo.ac.jp>
2018-08-22 17:39:50 +0900

    update

--- simulator/buildAgents.sh
+++ simulator/buildAgents.sh
@@ -1,11 +1,11 @@
 cp source/NNAgent_evaluation.hpp source/mainAI.hpp
-g++ source/mainAI.cpp  -o NNAgent_evaluation
+g++ source/mainAI.cpp  -o NNAgent_evaluation -std=c++11
 
 cp source/NNAgent_sampling.hpp source/mainAI.hpp
-g++ source/mainAI.cpp  -o NNAgent_sampling
+g++ source/mainAI.cpp  -o NNAgent_sampling -std=c++11
 
 cp source/NoNNAgent_sampling.hpp source/mainAI.hpp
-g++ source/mainAI.cpp  -o NoNNAgent_sampling
+g++ source/mainAI.cpp  -o NoNNAgent_sampling -std=c++11
 
 cp source/NoNNAgent_evaluation.hpp source/mainAI.hpp
-g++ source/mainAI.cpp  -o NoNNAgent_evaluation
+g++ source/mainAI.cpp  -o NoNNAgent_evaluation -std=c++11
