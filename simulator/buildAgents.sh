facb53c8021b9c32e4ccf2fd2288fe8cf744c601
ODA <twbtarai.blackpepper@gmail.com>
2018-08-23 18:43:59 +0900

    add g

--- simulator/buildAgents.sh
+++ simulator/buildAgents.sh
@@ -1,13 +1,13 @@
 cp source/NNAgent_evaluation.hpp source/mainAI.hpp
-g++ source/mainAI.cpp  -o NNAgent_evaluation -std=c++11
+g++ source/mainAI.cpp  -o NNAgent_evaluation -std=c++14
 
 cp source/NNAgent_sampling.hpp source/mainAI.hpp
-g++ source/mainAI.cpp  -o NNAgent_sampling -std=c++11
+g++ source/mainAI.cpp  -o NNAgent_sampling -std=c++14
 
 cp source/NoNNAgent_sampling.hpp source/mainAI.hpp
-g++ source/mainAI.cpp  -o NoNNAgent_sampling -std=c++11
+g++ source/mainAI.cpp  -o NoNNAgent_sampling -std=c++14
 
 cp source/NoNNAgent_evaluation.hpp source/mainAI.hpp
-g++ source/mainAI.cpp  -o NoNNAgent_evaluation -std=c++11
+g++ source/mainAI.cpp  -o NoNNAgent_evaluation -std=c++14
 
-g++ ../logAnalyzer/logparser.cpp -o logparser -std=c++11
+g++ ../logAnalyzer/logparser.cpp -o logparser -std=c++14
